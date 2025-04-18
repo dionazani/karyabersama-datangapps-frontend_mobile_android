ca24223a68fa118e034591389039ead0fdd6c59d
Dion Azani <dion.azani@gmail.com>
2025-04-18 22:18:52 +0700

    Create login page

--- app/src/main/java/org/karyabersama/datangapps/MainActivity.java
+++ app/src/main/java/org/karyabersama/datangapps/MainActivity.java
@@ -1,6 +1,10 @@
 package org.karyabersama.datangapps;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText usernameEditText;
+    EditText passwordEditText;
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,21 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        usernameEditText = findViewById(R.id.usernameEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               var username = usernameEditText.getText();
+               var password = passwordEditText.getText();
+
+                Log.i("Test Credentials", String.format("Username : %s and Password : %s", username, password));
+            }
+        }
+
+        );
     }
 }
